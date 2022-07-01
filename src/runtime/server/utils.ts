2f44ea1c061a4d2a9d8092b64f2169c6483e6d16
Yaël Guilloux <yael.guilloux@gmail.com>
2022-07-02 00:23:59 +0200

    fix(server): avoid importing outside of server context

--- src/runtime/server/utils.ts
+++ src/runtime/server/utils.ts
@@ -1,7 +1,33 @@
 import type { Core as Instance } from 'browser-style-dictionary/types/browser'
 import StyleDictionary from 'browser-style-dictionary/browser.js'
 import type { NuxtThemeTokens } from '../../index'
-import { objectPaths } from '../../utils'
+
+/**
+ * Make a list of `get()` compatible paths for any object
+ */
+export const objectPaths = (data: any) => {
+  const output: any = []
+  function step (obj: any, prev?: string) {
+    Object.keys(obj).forEach((key) => {
+      const value = obj[key]
+      const isarray = Array.isArray(value)
+      const type = Object.prototype.toString.call(value)
+      const isobject =
+        type === '[object Object]' ||
+        type === '[object Array]'
+
+      const newKey = prev
+        ? `${prev}.${key}`
+        : key
+
+      if (!output.includes(newKey)) { output.push(newKey) }
+
+      if (!isarray && isobject && Object.keys(value).length) { return step(value, newKey) }
+    })
+  }
+  step(data)
+  return output
+}
 
 const DesignTokenType =
 `interface DesignToken {
